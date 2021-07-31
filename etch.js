551fbe2d4c29c5253f8aa03a5b1b1ff191e4f520
bananazigzags <bananazigzags@gmail.com>
2021-07-31 23:02:28 +0700

    added event listeners to change color on mouseover, clear button with prompt

--- etch.js
+++ etch.js
@@ -8,3 +8,26 @@ for (i = 0; i < 16 * 16; i++) {
     square.setAttribute("id", `square${i+1}`);
     container.appendChild(square);
 }
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => askSize());
+
+function askSize() {
+    let gridSize = parseInt(prompt('Enter grid size (3-20)'));
+    alert(`You chose ${gridSize}x${gridSize} grid`);
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "purple";
+    });
+});
+
+function clearGrid() {
+    squares.forEach((square) => {
+        square.style.backgroundColor = "white";
+    });
+};
+
+btn.addEventListener('click', clearGrid);
