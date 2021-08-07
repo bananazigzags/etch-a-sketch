cee230c18bd157a464c5f258f40ff2b3923f6257
bananazigzags <bananazigzags@gmail.com>
2021-08-07 21:22:39 +0700

    added resize

--- etch.js
+++ etch.js
@@ -1,33 +1,53 @@
 
 const container = document.querySelector('#container');
 
-for (i = 0; i < 16 * 16; i++) {
-    
-    const square = document.createElement('div');
-    square.classList.add(`square`);
-    square.setAttribute("id", `square${i+1}`);
-    container.appendChild(square);
+function newGrid(gridSize) {
+    for (i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add(`square`);
+        square.setAttribute("id", `square${i + 1}`);
+        container.appendChild(square);
+        container.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr); 
+        grid-template-rows: repeat(${gridSize}, 1fr);`);
+
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = getRandomColor();
+            });
+        });
+    }
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => askSize());
+newGrid(16);
+
+function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+}
 
 function askSize() {
-    let gridSize = parseInt(prompt('Enter grid size (3-20)'));
-    alert(`You chose ${gridSize}x${gridSize} grid`);
+    let size = 0;
+    while (size < 3 || size > 30) {
+        size = parseInt(prompt('Enter grid size (3-30)'));
+    }
+    container.innerHTML = '';
+    newGrid(size);
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "purple";
-    });
-});
-
 function clearGrid() {
+    const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.style.backgroundColor = "white";
     });
 };
 
-btn.addEventListener('click', clearGrid);
+const resize = document.querySelector('#resize');
+resize.addEventListener('click', () => askSize());
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => clearGrid());
